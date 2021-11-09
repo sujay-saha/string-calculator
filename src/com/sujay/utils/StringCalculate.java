18f71da84a99fd069bd3a0bbc96cba088a439ec2
Sujay Saha <sujays705@gmail.com>
2021-11-09 09:38:10 +0530

    Test cases segregated and added functionality for adding two values.

--- src/com/sujay/utils/StringCalculate.java
+++ src/com/sujay/utils/StringCalculate.java
@@ -3,6 +3,13 @@ package com.sujay.utils;
 public class StringCalculate {
 
     public int add(String numbers) {
+        if(numbers!=null && !numbers.isEmpty()){
+            String[] numberArr = numbers.split(",");
+            if(numberArr.length==1)
+                return Integer.parseInt(numberArr[0]);
+            else
+                return Integer.parseInt(numberArr[0])+Integer.parseInt(numberArr[1]);
+            }
         return 0;
     }
 }
--- test/com/sujay/utils/TestStringCalculate.java
+++ test/com/sujay/utils/TestStringCalculate.java
@@ -1,14 +1,31 @@
 package com.sujay.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestStringCalculate {
 
-    @Test
-    void testEmptyValSum(){
-            StringCalculate stringCalc = new StringCalculate();
-            assertEquals(0, stringCalc.add(""));
+    @ParameterizedTest(name = "{index}. Input:\"{0}\" , Expected o/p: \"0\"")
+    @EmptySource
+    void testEmptyValAdd(String actual){
+        StringCalculate stringCalc = new StringCalculate();
+        assertEquals(0, stringCalc.add(actual));
+    }
+
+    @ParameterizedTest(name = "{index}. Input:\"{0}\" , Expected o/p: \"{1}\"")
+    @CsvSource(value = {"1:1","0:0","100:100","1000000000:1000000000","2147483647:2147483647"},delimiter = ':')
+    void testSingleValAdd(String actual,int expected){
+        StringCalculate stringCalc = new StringCalculate();
+        assertEquals(expected, stringCalc.add(actual));
+    }
+
+    @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
+    @CsvSource(value = {"1,2:3","0,0:0","0,1:1","10,10:20","-1,-1:-2","2147473647,10000:2147483647"},delimiter = ':')
+    void testMultiValAdd(String actual,int expected){
+        StringCalculate stringCalc = new StringCalculate();
+        assertEquals(expected, stringCalc.add(actual));
     }
 }
