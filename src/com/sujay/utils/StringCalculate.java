b40532c1214c2bb6ce3670689222efdef1c8ed70
Sujay Saha <sujays705@gmail.com>
2021-11-10 23:20:32 +0530

    Added support for handling customized multiple delims, Simplified extraction of header or delims and values part,Added regex to support delims containing special regex characters.

--- src/com/sujay/utils/StringCalculate.java
+++ src/com/sujay/utils/StringCalculate.java
@@ -1,50 +1,69 @@
 package com.sujay.utils;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringCalculate {
 
     public int add(String numbers) {
         int result = 0;
 
-        if (numbers != null && numbers.length() != 0) {
+        if (checkNumberStrNotNullAndNotEmpty(numbers)) {
+
             String[] numberArr;
             String delim = "[\n,]";
+
             if (numbers.startsWith("/")) {
-                int startIndex = 2;
-                int endIndex = numbers.indexOf("\n");
+                String[] headAndValues = numbers.split("\n",2);
+                String delims = headAndValues[0].substring(2);
 
-                delim = numbers.substring(startIndex, endIndex);  //numbers.charAt(2) was used previously, modified if delim size increases
+                if(delims.charAt(0)=='[') {
+                    delims = delims.substring(1, delims.length() - 1);
+                }
 
-                numbers = numbers.substring(endIndex + 1);
-                numberArr = numbers.split("[\n" + delim + "]");
+                delim = Stream.of(delims.split("]\\["))
+                            .map(Pattern::quote)                    //\Q and \E would be added to the string
+                                                                    // restrict the special chars functionality and act
+                                                                    // as normal strings
+                            .collect(Collectors.joining("|"));
+                delim = delim+"|\n";
 
+                numbers = headAndValues[1];
+                numberArr = numbers.split(delim);
             } else {
                 numberArr = numbers.split(delim);
             }
 
-            for (String val : numberArr) {
-                if (val.length() != 0) {
+            for (String strVal : numberArr) {
+                if (strVal.length() != 0) {
 
                     try {
-                        if(Integer.parseInt(val)>-1)
-                            result = Math.addExact(result, Integer.parseInt(val));   //addExact throws exception if any overflow/underflow would be there in integer addition#Java 1.8 or above reqd.
-                        else
-                            throw new NumberFormatException("negatives not allowed,Values_"+ Arrays.stream(numberArr)
+                        int val = Integer.parseInt(strVal);
+                        if( checkValNonNegative(val) ) {
+                            if (val < 1001)
+                                result = Math.addExact(result, val);   //addExact throws exception if any overflow/underflow would be there in integer addition#Java 1.8 or above reqd.
+                        }else
+                            throw new ArithmeticException("negatives not allowed,Values_"+ Arrays.stream(numberArr)
                                                                                             .filter(values -> Integer.parseInt(values)<0)
                                                                                             .collect(Collectors.joining(",")));
                     } catch (NumberFormatException e) {
-                        if(e.getMessage().startsWith("F"))
-                            throw new NumberFormatException("Value \"" + val + "\" is not in int range");
-                        else
-                            throw new NumberFormatException(e.getMessage());
+                        throw new NumberFormatException("Value \"" + strVal + "\" is not convertable to int range");
                     } catch (ArithmeticException e) {
-                        throw new ArithmeticException("Integer Overflow!");
+                        throw new ArithmeticException(e.getMessage());
                     }
                 }
             }
         }
         return result;
+    }
+
+    private boolean checkNumberStrNotNullAndNotEmpty(String str) {
+        return str != null && str.length() != 0;
+    }
+
+    private boolean checkValNonNegative(int value) {
+        return value>-1 ;
     }
 }
--- test/com/sujay/utils/TestStringCalculate.java
+++ test/com/sujay/utils/TestStringCalculate.java
@@ -26,7 +26,7 @@ class TestStringCalculate {
                 Arguments.of("1\n2,3", 6),
                 Arguments.of("100\n200,300", 600),
                 Arguments.of("100\n200,300\n8,6,8\n777,350", 1749),
-                Arguments.of("123\n456,879,12,473\n1123", 3066)
+                Arguments.of("123\n456,879,12,473\n1000\n123", 3066)
         );
     }
 
@@ -35,7 +35,16 @@ class TestStringCalculate {
                 Arguments.of("//;\n1;2",3),
                 Arguments.of("//:\n100\n200:300", 600),
                 Arguments.of("//.\n200.300\n8.6.8\n677", 1199),
-                Arguments.of("//$\n123\n456$879$12$473\n1123", 3066)
+                Arguments.of("//$\n123\n456$879$12$473\n1000\n123", 3066)
+        );
+    }
+
+    private static Stream<Arguments> provideStringsWithCustomizedMultiDelimChar() {  //Passing values to testMultiValWithCustomizedMultiDelim test method
+        return Stream.of(
+                Arguments.of("//[***][?]\n1***2?3",6),
+                Arguments.of("//[:][^^^][___]\n100\n200:300^^^400___500", 1500),
+                Arguments.of("//[...][..][.][{][?]\n200...300\n8..6.8\n67{7?11", 607),
+                Arguments.of("//[$$][%][&][#]\n123\n456$$879$$12473\n10%5&6\n123#12", 1614)
         );
     }
 
@@ -46,28 +55,27 @@ class TestStringCalculate {
     }
 
     @ParameterizedTest(name = "{index}. Input:\"{0}\" , Expected o/p: \"{1}\"")
-    @CsvSource(value = {"1:1", "0:0", "100:100", "1000000000:1000000000", "2147483647:2147483647"}, delimiter = ':')
+    @CsvSource(value = {"1:1", "0:0", "100:100", "999:999", "1000:1000"}, delimiter = ':')
     void testSingleValAdd(String actual, int expected) {
         assertEquals(expected, stringCalc.add(actual));
     }
 
     @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
-    @CsvSource(value = {"1,2:3", "0,0:0", "0,1:1", "10,10:20", "1,1:2", "2147473647,10000:2147483647"}, delimiter = ':')
+    @CsvSource(value = {"1,2:3", "0,0:0", "0,1:1", "10,10:20", "1,1:2", "1000,1000:2000"}, delimiter = ':')
     void testDoubleValAdd(String actual, int expected) {
         assertEquals(expected, stringCalc.add(actual));
     }
 
     @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
-    @CsvSource(value = {",2:2", "0,:0", ",10000000:10000000", "20,:20", "1,:1"}, delimiter = ':')
+    @CsvSource(value = {",2:2", "0,:0", ",1000:1000", "20,:20", "1,:1"}, delimiter = ':')
     void testDoubleValOneEmptyAdd(String actual, int expected) {
         assertEquals(expected, stringCalc.add(actual));
     }
 
     @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected Exception!!")
-    @CsvSource(value = {"9223372036854775808,0:Value \"9223372036854775808\" is not in int range",
-            "9223372036854775807,9223372036854775807:Value \"9223372036854775807\" is not in int range",
-            "2147482223647,-10222222000000:Value \"2147482223647\" is not in int range",
-            "2147483647,1:Integer Overflow!","2147483647,2147483647:Integer Overflow!"}, delimiter = ':')
+    @CsvSource(value = {"9223372036854775808,0:Value \"9223372036854775808\" is not convertable to int range",
+            "9223372036854775807,9223372036854775807:Value \"9223372036854775807\" is not convertable to int range",
+            "2147482223647,-10222222000000:Value \"2147482223647\" is not convertable to int range"}, delimiter = ':')
     void testDoubleValUnknownAmountOfNumbersAddCheck(String actual, String expectedMessage) {
         Exception thrown;
         if(expectedMessage.startsWith("V")){
@@ -80,9 +88,9 @@ class TestStringCalculate {
 
     @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
     @CsvSource(value = {"1,2,3:6", "0,1,100,200:301",
-            "10000000,10000000,100,200,300,190,1,10,20,30,40,50,1,2,3,4,5,6,7,8,9,1210," +
-            "20,30,40,50,1,2,3,4,5,6,7,8,9,12:20002393", "10,20,30,40,50,1,2,3,4,5,6,7,8,9,12:207",
-            "10,20,30,40,50,1,2,3,4,5,6,7,8,9,1210,20,30,40,50,1,2,3,4,5,6,7,8,9,12:1602"}, delimiter = ':')
+            "100,200,300,190,1,10,20,30,40,50,1,2,3,4,5,6,7,8,999," +
+            "20,30,40,50,1,278,3,4,75,6,7,8,9,12:2519", "10,20,30,40,50,1,2,3,4,5,6,7,8,9,12:207",
+            "10,20,30,40,50,1,2,3,4,5,6,7,8,9,1000,210,20,30,40,50,1,2,3,4,5,6,7,8,9,12:1602"}, delimiter = ':')
     //@CsvFileSource(resources = "/resources/Values.txt",delimiter = ':',maxCharsPerColumn=20000)
     void testMultiValAdd(String actual, int expected) {
         assertEquals(expected, stringCalc.add(actual));
@@ -96,7 +104,7 @@ class TestStringCalculate {
 
     @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
     @MethodSource("provideStringsWithCustomizedDelimChar")
-    void testMultiValWithCustomizedDelim(String actual, int expected) {
+    void testMultiValWithCustomizedSingleDelim(String actual, int expected) {
         assertEquals(expected, stringCalc.add(actual));
     }
 
@@ -104,7 +112,20 @@ class TestStringCalculate {
     @CsvSource(value = {"-1,213,-232,2,8,78,56,-75:negatives not allowed,Values_-1,-232,-75",
             "-1,21,-11,0,3,-232,2,8,-100,-1000000,78,56,-75:negatives not allowed,Values_-1,-11,-232,-100,-1000000,-75"},delimiter = ':')
     void testMultiValWithNegativeValCheck(String actual, String expectedMessage) {
-        Exception thrown = assertThrows(NumberFormatException.class, () -> stringCalc.add(actual));
+        Exception thrown = assertThrows(ArithmeticException.class, () -> stringCalc.add(actual));
         assertEquals(expectedMessage, thrown.getMessage());
     }
+
+    @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
+    @CsvSource(value = {"1000,2:1002", "12130,2133:0", "1000,1:1001", "1000,1000:2000","1000000,1000000:0"}, delimiter = ':')
+    void testMultiValSkipMoreThanThousandAdd(String actual, int expected) {
+        assertEquals(expected, stringCalc.add(actual));
+    }
+
+    @ParameterizedTest(name = "{index}. Input: \"{0}\" , Expected o/p: \"{1}\"")
+    @MethodSource("provideStringsWithCustomizedMultiDelimChar")
+    void testMultiValWithCustomizedMultiDelim(String actual, int expected) {
+        assertEquals(expected, stringCalc.add(actual));
+    }
+
 }
